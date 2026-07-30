482067392c6b9c228de1073dde19e9480d27a844
Moritz Mazetti <moritz@matchory.com>
2026-07-30 18:22:51 +0200

    build: format with the shared Pint preset

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', static fn (): array => ['status' => 'ok']);
+Route::get('/', static fn(): array => ['status' => 'ok']);
